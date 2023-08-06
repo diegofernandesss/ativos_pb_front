91284b05d03e6eafae4c59d50a46c3cd2357d5a5
Jardiel <Jardiel.CarlosM@hotmail.com>
2023-08-06 14:34:48 -0300

    versão mais estavel da paginação

--- src/pages/PaginaInicial.jsx
+++ src/pages/PaginaInicial.jsx
@@ -7,12 +7,14 @@ import { Main } from '../components/PaginaInicial/Main';
 export const PaginaInicial = () => {
     const [patente, setPatente] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false);
+    const [cur, setCur] = useState(1);
+    const [ictSelected, setIctSelected] = useState("")
 
     return (
         <>
             <NavBar  />
-            <Header updatePatente={setPatente} setRemoveLoading={setRemoveLoading}/>
-            <Main patente={patente} removeLoading={removeLoading} setRemoveLoading={setRemoveLoading}/>
+            <Header updatePatente={setPatente} setRemoveLoading={setRemoveLoading} currentPage={cur} updateIctSelected={setIctSelected}/>
+            <Main patente={patente} removeLoading={removeLoading} setRemoveLoading={setRemoveLoading} setCuror={setCur} ictSelected={ictSelected}/>
             <Footer />                  
         </>
     );
