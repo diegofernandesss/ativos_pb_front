d99f8da3db8b2b53b09070eb043dadae622b70bf
Jardiel <Jardiel.CarlosM@hotmail.com>
2023-08-12 10:51:46 -0300

    Finalize Pagination

--- src/pages/PaginaInicial.jsx
+++ src/pages/PaginaInicial.jsx
@@ -5,24 +5,14 @@ import { Main } from "../components/PaginaInicial/Main";
 
 export const PaginaInicial = () => {
 
-    const [patente, setPatente] = useState([]);
-    const [removeLoading, setRemoveLoading] = useState(false);
     const [ictSelected, setIctSelected] = useState("ICTs")
-    const [cur, setCur] = useState(1)
 
     return (
         <Layout>
             <Header 
-                updatePatente={setPatente} 
-                setRemoveLoading={setRemoveLoading} 
-                currentCursor={cur}
-                IctSelected={setIctSelected}
+                setIctSelectedMain={setIctSelected}
             />
             <Main 
-                patente={patente} 
-                removeLoading={removeLoading} 
-                setRemoveLoading={setRemoveLoading} 
-                setCursor={setCur}
                 ictSelected={ictSelected}
             />
         </Layout>
