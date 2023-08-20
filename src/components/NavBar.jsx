36d499befdfe5d0a6b2bae24eb0de9c23fadcc10
Diego Fernandes <fernandes.diego@academico.ifpb.edu.br>
2023-08-20 11:11:18 -0300

    Ajuste nas posições

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,37 +36,37 @@ return (
                         Ativos<LogoHighligh> PB</LogoHighligh>
                     </Logo>
 
-                <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                    {Links.map((link, index) => (
-                        <NavLink key={index} to={link.link} >
-                          {link.name}
-                        </NavLink>
-                    ))}
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            {Links.map((link, index) => (
+                                <NavLink key={index} to={link.link} >
+                                {link.name}
+                                </NavLink>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <div className="md:hidden flex items-center "> 
-                    <MobileMenuButton onClick={toggleMenu} className="ml-4">
-                        {menuOpen ? (
-                                <FiX className="h-6 w-6 text-white" />
-                            ) : ( 
-                                <FiMenu className="h-6 w-6 text-white" />
-                            )}
-                    </MobileMenuButton>
-                </div>
+                    <div className="md:hidden flex items-center "> 
+                        <MobileMenuButton onClick={toggleMenu} className="ml-4">
+                            {menuOpen ? (
+                                    <FiX className="h-6 w-6 text-white" />
+                                ) : ( 
+                                    <FiMenu className="h-6 w-6 text-white" />
+                                )}
+                        </MobileMenuButton>
+                    </div>
                 </Nav>
             </NavContainer>
-            {menuOpen && (
-                <div className="md:hidden">
-                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        {Links.map((link, index) => (
-                        <NavLinkMobile key={index} to={link.link}>
-                            {link.name}
-                        </NavLinkMobile>
-                        ))}
+                {menuOpen && (
+                    <div className="md:hidden">
+                        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            {Links.map((link, index) => (
+                            <NavLinkMobile key={index} to={link.link}>
+                                {link.name}
+                            </NavLinkMobile>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
         </NavBackground>
     </>
 
