34dd79c8c8ef03b91a31f80ec3894baf9d490350
Diego Fernandes <fernandes.diego@academico.ifpb.edu.br>
2023-08-23 16:13:01 -0300

    Alteração nos selects

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -180,7 +180,7 @@ export const Main = ({ ictSelected, resultNumPatente, removeLoading, setRemoveLo
                             ))}
                         </Select>
                         <Select >
-                            <option value="option1">Sub Seção</option>
+                            <option value="option1">Sub Seção lsmdsmd assamsaç sa,saçs,a</option>
                             <option value="option1">Option 1</option>
                             <option value="option2">Option 2</option>
                             <option value="option3">Option 3</option>
